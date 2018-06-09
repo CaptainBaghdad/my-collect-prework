694387f6bbe558354c88c0a43ab38d5d93f8f82a
Jason Pitts <juniorjr083@gmail.com>
2018-06-09 13:14:24 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(lang)
+  na = []
+  num = 0 
+  while num < lang.length 
+  
+  if block_given? na << yield(lang[num])
+  
+  num +=1  
+    end
+   end 
+       
+lang
+  
+  
+end 
+
+#my_collect(a) {|e| e.upcase}
+#my_collect(students) {|s| s.split(" ").first}
+
 
 
